8601ae25cd352a9e0706d7f9d471ff571e97b425
Weishler Berman <ilcreatore321@gmail.com>
2024-06-11 15:30:20 -0400

    Creates Footer Component on Home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,45 +5,140 @@ import {
   Box,
   List,
   ListItem,
+  ListItemIcon,
   ListItemText,
+  useTheme,
 } from "@mui/material";
 import NetcomLogo from "./NetcomLogo";
 import { Link } from "react-router-dom";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import XIcon from "@mui/icons-material/X";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 const Footer = () => {
+  const services = [
+    {
+      id: "0",
+      label: "Residencial Inalámbrico",
+      href: "#",
+    },
+    {
+      id: "1",
+      label: "Pyme Inalámbrico",
+      href: "#",
+    },
+    {
+      id: "2",
+      label: "Residencial Fibra Optica",
+      href: "#",
+    },
+    {
+      id: "3",
+      label: "Pyme Fibra Optica",
+      href: "#",
+    },
+    {
+      id: "4",
+      label: "Community",
+      href: "#",
+    },
+    {
+      id: "5",
+      label: "Empresarial Dedicado",
+      href: "#",
+    },
+  ];
 
-    const services = [
-        {
-            id: '0',
-            label: 'Residencial Inalambrico',
-            href: '#'
-        },
-        {
-            id: '1',
-            label: 'Pyme Inalambrico',
-            href: '#'
-        },
-        {
-            id: '2',
-            label: 'Residencial Fibra Optica',
-            href: '#'
-        },
-        {
-            id: '3',
-            label: 'Pyme Fibra Optica',
-            href: '#'
-        },
-        {
-            id: '4',
-            label: 'Community',
-            href: '#'
-        },
-        {
-            id: '5',
-            label: 'Empresarial Dedicado',
-            href: '#'
-        },
-    ]
+  const socials = [
+    {
+      id: "0",
+      label: "Instagram",
+      icon: <InstagramIcon />,
+      href: "https://www.instagram.com/netcomplus.ve/",
+    },
+    {
+      id: "1",
+      label: "Facebook",
+      icon: <FacebookIcon />,
+      href: "https://www.facebook.com/people/NetCom-Plus/100063485411131/",
+    },
+    {
+      id: "2",
+      label: "X",
+      icon: <XIcon />,
+      href: "https://x.com/i/flow/login?redirect_after_login=%2Fnetcomplusve",
+    },
+    {
+      id: "3",
+      label: "Linkedin",
+      icon: <LinkedInIcon />,
+      href: "https://ve.linkedin.com/company/netcomplusve",
+    },
+    {
+      id: "4",
+      label: "Whatsapp",
+      icon: <WhatsAppIcon />,
+      href: "https://api.whatsapp.com/send?phone=584244126217",
+    },
+  ];
+
+  const platforms = [
+    {
+      id: "0",
+      label: "Portal de Pagos",
+      href: "https://pagos.netcomplusve.com:445/portal-de-pago/",
+    },
+  ];
+
+  const generateList = (title, list) => {
+    return (
+      <>
+        <Typography color="textPrimary" variant="h6" component="span">
+          {title}
+        </Typography>
+        <List
+          dense
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {list.map((li) => (
+            <ListItem
+              key={li.id}
+              sx={{
+                "&:hover": {
+                  cursor: "pointer",
+                  color: theme.palette.primary.main,
+                },
+              }}
+            >
+              <Link
+                to={li.href}
+                target="_blank"
+                style={{
+                  textDecoration: "none",
+                  color: "inherit",
+                  textWrap: "nowrap",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "10px",
+                }}
+              >
+                {li.icon ? li.icon : null}
+                <ListItemText primary={li.label} />
+              </Link>
+            </ListItem>
+          ))}
+        </List>
+      </>
+    );
+  };
+
+  const theme = useTheme();
 
   return (
     <Paper
@@ -64,6 +159,7 @@ const Footer = () => {
         }}
       >
         <Grid
+          item
           xs={2}
           sx={{
             display: "flex",
@@ -73,58 +169,17 @@ const Footer = () => {
         >
           <NetcomLogo></NetcomLogo>
         </Grid>
-        <Grid xs={2}>
-          <Typography color="textPrimary" variant="h6">
-            Servicios
-          </Typography>
-          <List
-          dense
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <ListItem>
-            <Link><ListItemText primary="Residencial Inalambrico" /></Link>
-            </ListItem>
-          </List>
+        <Grid item xs={2}>
+          {generateList("Servicios", services)}
         </Grid>
-        <Grid
-          xs={2}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography color="textPrimary" variant="subtitle1">
-            Informacion
-          </Typography>
+        <Grid item xs={2}>
+          {generateList("Servicios", services)}
         </Grid>
-        <Grid
-          xs={2}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography color="textPrimary" variant="subtitle1">
-            asd
-          </Typography>
+        <Grid item xs={2}>
+          {generateList("Plataforma", platforms)}
         </Grid>
-        <Grid
-          xs={2}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography color="textPrimary" variant="subtitle1">
-            Redes
-          </Typography>
+        <Grid item xs={2}>
+          {generateList("Nuestras Redes", socials)}
         </Grid>
       </Grid>
       <Box
@@ -132,7 +187,6 @@ const Footer = () => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          paddingX: "30px",
         }}
       >
         <Typography color="textSecondary" variant="subtitle2">
