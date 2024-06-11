abd819a1b48d1a39bb0987f6a11a41fb83e838e3
Weishler Berman <ilcreatore321@gmail.com>
2024-06-11 12:45:33 -0400

    Add Routing with React Router

---
+++ src/Router.jsx
@@ -0,0 +1,55 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// Routes
+import App from "../src/App";
+import Home from "./Routes/Home";
+import AboutUs from "./Routes/AboutUs";
+import Coverage from "./Routes/Coverage";
+import Services from "./Routes/Services";
+import Faqs from "./Routes/Faqs";
+import Contact from "./Routes/Contact";
+import ErrorPage from "./Routes/ErrorPage";
+
+const Router = createBrowserRouter([
+  // Root
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      // Home
+      {
+        path: "/inicio",
+        element: <Home />,
+        errorElement: <ErrorPage />,
+      },
+      // AboutUs
+      {
+        path: "/sobre-nosotros",
+        element: <AboutUs />,
+      },
+      // Coverage
+      {
+        path: "/cobertura",
+        element: <Coverage />,
+      },
+      // Services
+      {
+        path: "/servicios",
+        element: <Services />,
+      },
+      // Faqs
+      {
+        path: "/faqs",
+        element: <Faqs />,
+      },
+      // Contact
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
+
+export default Router;
--- src/ThemeContextProvider.jsx
+++ src/ThemeContextProvider.jsx
@@ -4,13 +4,16 @@ import {
   createTheme,
 } from "@mui/material/styles";
 import { createContext, useMemo, useState } from "react";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export const ThemeContext = createContext({
   switchColorMode: () => {},
 });
 
 export function ThemeContextProvider({ children }) {
-  const [mode, setMode] = useState("light")
+  
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = useState(prefersDarkMode ? 'dark' : 'light')
 
   const switchColorMode = () => {
     setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
